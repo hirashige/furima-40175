d10909eeea762971b7e14a8df65129850522452e
hirashige <rstoyo.shigemitsu.hirano@gmail.com>
2023-12-19 12:16:26 +0900

    不要な実装のコメントアウト

--- db/migrate/20231215061211_devise_create_users.rb
+++ db/migrate/20231215061211_devise_create_users.rb
@@ -16,8 +16,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
 
 
       ## Recoverable
-      t.string   :reset_password_token
-      t.datetime :reset_password_sent_at
+      # t.string   :reset_password_token
+      # t.datetime :reset_password_sent_at
 
       ## Rememberable
       t.datetime :remember_created_at
@@ -45,7 +45,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
     end
 
     add_index :users, :email,                unique: true
-    add_index :users, :reset_password_token, unique: true
+    # add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
   end
