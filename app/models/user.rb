3e3bb3546e2d2c3fb6574b22fb7f225de2a2805e
hirashige <rstoyo.shigemitsu.hirano@gmail.com>
2023-12-20 17:46:12 +0900

    ActiveHashの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :items
+
   validates :nickname, presence: true
-  validates :family_name, presence: true 
+  validates :family_name, presence: true
   validates :first_name, presence: true
   validates :family_name_kana, presence: true
   validates :first_name_kana, presence: true
   validates :birthday, presence: true
-  
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   VALID_NAME_REGEX = /\A[ぁ-んァ-ヶ一-龥々ー]+\z/
   VALID_NAME_KANA_REGEX =/\A[ァ-ヶー－]+\z/
