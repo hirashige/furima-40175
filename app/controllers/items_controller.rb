119a9664a10cb4db598b7d0cf5aef3507ff3badc
hirashige <rstoyo.shigemitsu.hirano@gmail.com>
2023-12-25 17:01:28 +0900

    メソッド修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -46,9 +46,8 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    unless current_user.id == @item.user_id
+    return if current_user.id == @item.user_id
 
-      redirect_to action: :index
-    end
+    redirect_to action: :index
   end
 end
