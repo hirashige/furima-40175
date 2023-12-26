95b0370ef106445a5510ff377364618dc0fc0560
hirashige <rstoyo.shigemitsu.hirano@gmail.com>
2023-12-26 17:00:04 +0900

    Formオブジェクトの作成

---
+++ app/models/address_history.rb
@@ -0,0 +1,29 @@
+class AddressHistory
+  include ActiveModel::Model
+  attr_accessor :post_code, :prefecture_id, :city, :address, :building_name, :telephone, :user_id, :item_id
+
+  with_options presence: true do
+    validates :user_id
+    validates :item_id
+    validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
+    validates :prefecture_id
+    validates :city
+    validates :address
+    validates :telephone
+  end
+
+  def save
+    history = History.create(
+      item_id,
+      user_id
+    )
+    ShoppingAddress.create(
+      post_code,
+      prefecture_id,
+      city,
+      address,
+      telephone,
+      history_id: history.id
+    )
+  end
+end
